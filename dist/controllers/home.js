1c46efd23f096202b8d85701be3eba03f973baba
Matthew Jeorrett <Matthew@wallet.services>
2018-02-02 20:40:11 +0000

    remove dist folder

--- dist/controllers/home.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.index = function (req, res) {
-    res.send("hello world!");
-};
-//# sourceMappingURL=home.js.map
