49e400495a99e388edc13a03704ac01c2bb639f3
Matthew Jeorrett <Matthew@wallet.services>
2018-02-28 21:42:22 +0000

    initial implementation of firebase

--- server.js
+++ server.js
@@ -3,13 +3,39 @@ const url = require('url');
 const fs = require('fs');
 const path = require('path');
 
+const firebase = require('firebase');
+const credentials = require('./credentials')
+
 const routes = require('./routes');
+
+var firebaseConfig = {
+    apiKey: credentials.firebase.apiKey,
+    authDomain: "nine-bits-in-a-byte.firebaseapp.com",
+    databaseURL: "https://nine-bits-in-a-byte.firebaseio.com",
+    projectId: "nine-bits-in-a-byte",
+    storageBucket: "",
+    messagingSenderId: credentials.firebase.messagingSenderId
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var firebaseLoggedIn = false;
+
+firebase.auth().onAuthStateChanged(function(user) {
+    firebaseLoggedIn = !!user;
+})
 
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
 
     const uri = url.parse(req.url).path.split("?")[0];
+    console.log("request uri:", uri)
+
+    if (uri === "/api/todos") {
+        fetchAndReturnTodos(res);
+        return;
+    }
 
     res.setHeader('Content-Type', 'text/html');
 
@@ -39,6 +65,35 @@ function writeFileForUri(uri, res) {
             resolve();
         });
     })
+}
+
+function fetchAndReturnTodos(res) {
+
+    if (firebaseLoggedIn) {
+        var db = firebase.database();        
+
+        db.ref('todos').once('value').then(function(snapshot) {
+            res.write(JSON.stringify(snapshot.val()));
+            res.end();
+        });
+    }
+    else {
+        console.log("logging in to firebase");
+        firebase.auth().signInWithEmailAndPassword("ninebitsinabyte@gmail.com", "Sitekit123")
+            .then(function() {
+                var db = firebase.database();
+                
+                db.ref('todos').once('value').then(function(snapshot) {
+                    res.write(JSON.stringify(snapshot.val()));
+                    res.end();
+                });
+            })
+            .catch(function(error) {
+                console.error("Error authenticating with Firebase:\n" +
+                    "code: " + error.code + "\n" +
+                    "message: " + error.message);
+            });
+    }
 }
 
 server.listen(port, () => {
